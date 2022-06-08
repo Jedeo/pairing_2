2360c670f86ceb473c0bbfe9fc2d9e4de86cd18d
Eric Weissman <eric@example.com>
2022-06-08 17:08:49 -0600

    Add elseif statmen

--- friends.js
+++ friends.js
@@ -1,7 +1,11 @@
 var s1 = "jedeo";
 
-if(s1 == "Dan"){
-  console.log("How are you doing");
-} else {
+var s2 = "jedi";
+
+if(s1 === "Dan"){
+  console.log("How are you doing " + s1);
+} else if ( s2 === "Dan2") {
+  console.log("How are you doing " + s2);
+}else {
   console.log("who are you?");
 }
